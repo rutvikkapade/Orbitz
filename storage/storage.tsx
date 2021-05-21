3435f6ff512a8738091080f041d949f045a00cfa
Rutvik Kapade <rutvik609@gmail.com>
2021-05-21 17:24:03 +0530

    1000

---
+++ storage/storage.tsx
@@ -0,0 +1,18 @@
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+export const storage=new Storage({
+    size : 1000,
+    storageBackend: AsyncStorage,
+    defaultExpires : null,
+    enableCache : true,
+})
+storage.save({
+    key : 'loginInfo',
+    data : {
+        username : null,
+        loggedIn : false,
+
+    }
+})
